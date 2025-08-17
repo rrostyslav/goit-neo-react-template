17a0f6c4130445e4ce4e971913687ccb1ee2f367
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-08-17 16:22:14 +0300

    initialized repository

---
+++ README.md
@@ -0,0 +1,37 @@
+# GoIT Homework
+
+## Tech Stack
+
+- Language: JavaScript
+- UI/Framework/Library: React
+- Build/Tooling: Vite ESLint, Prettier
+- Package manager: npm
+
+## Quick Start
+
+Prerequisites:
+
+- Node.js ≥ 18.x
+- npm ≥ 9.x
+
+Setup:
+
+1. Clone the repository
+   - git clone [repo-url]
+   - cd [project-folder]
+2. Install dependencies
+   - npm install
+
+Run:
+
+- Development: npm run dev
+- Production build: npm run build
+- Preview built app: npm run preview
+
+## Available Scripts
+
+- npm run dev — Start a local development server with hot reload
+- npm run build — Create an optimized production build
+- npm run preview — Preview the production build locally
+- npm run lint — Run ESLint checks
+- npm run format — Format code with Prettier
---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/indent': ['error', 2, {
+        SwitchCase: 1,
+        VariableDeclarator: 1,
+        outerIIFEBody: 1,
+      }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+]);
